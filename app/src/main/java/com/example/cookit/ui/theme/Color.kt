bb9819c9586e66dd11e6e0980794429028520f7e
SurajM <surajmargale777@gmail.com>
2025-09-12 17:12:46 +0530

    refactored ui and project structure

--- app/src/main/java/com/example/cookit/ui/theme/Color.kt
+++ app/src/main/java/com/example/cookit/ui/theme/Color.kt
@@ -2,6 +2,6 @@ package com.example.cookit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PrimaryColor = Color(0xFFFF695E)
+val PrimaryColor = Color(0xFFFF3E31)
 val SecondaryColor = Color(0xFFFFEB3B)
 val White = Color(0xFFFFFFFF)
